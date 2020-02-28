36ca607d3b23b6c997841611de6a544c6916662f
tkrogers3 <tkrogers3@gmail.com>
2020-02-27 23:14:53 -0500

    First Master Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//JS needed for the TOOLTIP and POPOVER:
+// Tooltip
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+// Popover
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
